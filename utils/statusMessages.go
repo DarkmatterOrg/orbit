544a887393cb907f12bf326751d890af196132bd
Alxhr0 <alxhr0@proton.me>
2025-03-09 16:37:44 +0100

    Fix conflict

--- utils/statusMessages.go
+++ utils/statusMessages.go
@@ -6,58 +6,57 @@ import (
 	"github.com/fatih/color"
 )
 
-var notice = color.New(color.FgCyan, color.Bold).SprintFunc()
-var info = color.New(color.FgBlue, color.Bold).SprintFunc()
-var error = color.New(color.FgRed, color.Bold).SprintFunc()
-var warning = color.New(color.FgYellow, color.Bold).SprintFunc()
-var done = color.New(color.FgGreen, color.Bold).SprintFunc()
-var debug = color.New(color.FgHiCyan, color.Bold).SprintFunc()
+var notice_msg = color.New(color.FgCyan, color.Bold).SprintFunc()
+var info_msg = color.New(color.FgBlue, color.Bold).SprintFunc()
+var error_msg = color.New(color.FgRed, color.Bold).SprintFunc()
+var warning_msg = color.New(color.FgYellow, color.Bold).SprintFunc()
+var done_msg = color.New(color.FgGreen, color.Bold).SprintFunc()
+var debug_msg = color.New(color.FgHiCyan, color.Bold).SprintFunc()
 
 func Notice(msg string) {
-	fmt.Println(notice("NOTICE") + ": " + msg)
+	fmt.Println(notice_msg("NOTICE") + ": " + msg)
 }
 
 func Noticef(msg string) string {
-	return fmt.Sprintf(notice("NOTICE") + ": " + msg)
+	return fmt.Sprintf(notice_msg("NOTICE") + ": " + msg)
 }
 
 func Info(msg string) {
-
-	fmt.Println(info("INFO") + ": " + msg)
+	fmt.Println(info_msg("INFO") + ": " + msg)
 }
 
 func Infof(msg string) string {
-	return fmt.Sprintf(info("INFO") + ": " + msg)
+	return fmt.Sprintf(info_msg("INFO") + ": " + msg)
 }
 
 func Error(msg string) {
-	fmt.Println(error("ERR") + ": " + msg)
+	fmt.Println(error_msg("ERR") + ": " + msg)
 }
 
 func Errorf(msg string) string {
-	return fmt.Sprintf(error("ERROR") + ": " + msg)
+	return fmt.Sprintf(error_msg("ERROR") + ": " + msg)
 }
 
 func Warn(msg string) {
-	fmt.Println(warning("WARN") + ": " + msg)
+	fmt.Println(warning_msg("WARN") + ": " + msg)
 }
 
 func Warnf(msg string) string {
-	return fmt.Sprintf(warning("WARN") + ": " + msg)
+	return fmt.Sprintf(warning_msg("WARN") + ": " + msg)
 }
 
 func Done(msg string) {
-	fmt.Println(done("DONE") + ": " + msg)
+	fmt.Println(done_msg("DONE") + ": " + msg)
 }
 
 func Donef(msg string) string {
-	return fmt.Sprintf(done("DONE") + ": " + msg)
+	return fmt.Sprintf(done_msg("DONE") + ": " + msg)
 }
 
 func Debug(msg string) {
-	fmt.Println(debug("DEBUG") + ": " + msg)
+	fmt.Println(debug_msg("DEBUG") + ": " + msg)
 }
 
 func Debugf(msg string) string {
-	return fmt.Sprintf(debug("DEBUG") + ": " + msg)
+	return fmt.Sprintf(debug_msg("DEBUG") + ": " + msg)
 }
